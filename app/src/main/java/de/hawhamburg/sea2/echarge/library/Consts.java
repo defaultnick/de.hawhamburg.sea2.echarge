08137b5764ef1421ce5229196fc1e19d8177ed19
Simon <simon.kallweit@haw-hamburg.de>
2014-05-11 15:58:31 +0200

    maps

--- app/src/main/java/de/hawhamburg/sea2/echarge/library/Consts.java
+++ app/src/main/java/de/hawhamburg/sea2/echarge/library/Consts.java
@@ -4,7 +4,7 @@ package de.hawhamburg.sea2.echarge.library;
  * Created by Simon on 10.05.2014.
  */
 public class Consts {
-    public static final String netCheckUrl = "http://192.168.253.10/ws/app_api/";
-    public static final String apiURL = "http://192.168.253.10/ws/app_api/2";
+    public static final String netCheckUrl = "http://192.168.253.10/ws/app_api2/";
+    public static final String apiURL = "http://192.168.253.10/ws/app_api/22";
     public static final String LoginDatei = "FirstActivity";
 }
